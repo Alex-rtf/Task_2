ede5aeaa7a5b8d81b8cc7c091b50e99eb3f7d22a
Alex-rtf <alex.lucky31@gmail.com>
2024-03-11 21:10:22 +0200

    Outputs Add

--- outputs.tf
+++ outputs.tf
@@ -4,9 +4,6 @@ output "public_ip" {
 }
 
 //
-output "aws_account_name" {
-  value = data.aws_caller_identity.current.account_id
-}
 
 output "caller_arn" {
   value = data.aws_caller_identity.current.arn
