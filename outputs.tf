396ec36786048e51dafa6cd2ef6a71658877f61b
Alex-rtf <alex.lucky31@gmail.com>
2024-03-11 22:27:20 +0200

    Task_3_Fixed_Output

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "public_ip" {
   value = aws_instance.myInstance.public_ip
 }
 
-//
+data "aws_caller_identity" "current" {}
 
 output "caller_arn" {
   value = data.aws_caller_identity.current.arn
